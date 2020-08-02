7c87c3f2abd33a7f2a8af6098933fc1ed178c8fc
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-02 15:50:56 +0000

    Dodanie wish_list, pokazujaca indexowanie list

---
+++ wish_list.py
@@ -0,0 +1,10 @@
+books = [
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+#the last element of every list, independent of their lenght
+print(books[len(books)-1])
