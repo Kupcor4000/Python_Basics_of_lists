e7e72dc3dcf698816741b7ab3f134b4fe2090da6
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-03 10:22:56 +0000

    Aktualizacja split.py o nowe funkcje i import time

--- split.py
+++ split.py
@@ -1,4 +1,11 @@
+import time
+
 quote = "There is no such thing as a moral or an immoral book. Books are well written, or badly written. That is all"
 quote2 =["All","art","is","quite","useless"]
 sentence = quote.split()
-print(sentence)
+sentence2 = " ".join(quote2)
+
+for word in sentence:
+    print(word)
+    time.sleep(0.1)
+print("\n"+sentence2)
