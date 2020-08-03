ce771222a34f8f2c16013122b9ea2c03a5ae9999
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-03 11:34:09 +0000

    Add function, simplify program

--- simply_aplication.py
+++ simply_aplication.py
@@ -1,6 +1,18 @@
 #Shooping list
 import time
 import os
+
+#funkcja polegajaca na dodawaniu obiektów
+def add_to_list(list1):
+    user_item = input("Please type a name of product which do you want to add to your list: ")
+    list1.append(user_item)
+    
+#funkcja pokazujaca aktualna zawartosc listy
+def show_list(list2):
+    for i in range(len(list2)):
+            print("{} : {}".format(i+1,list2[i]))
+    
+
 CONDITION = "DONE"
 ADD = "ADD"
 SHOW = "SHOW"
@@ -27,21 +39,19 @@ while user_input != CONDITION:
         
     #TODO User shoudl be able to add an item to the list
     elif user_input == ADD:
-        user_item = input("Please type a name of product which do you want to add to your list: ")
-        shopping_list.append(user_item)
+        add_to_list(shopping_list)
         os.system("clear")
         
     #TODO User should see a actual list at any time so that he can veryfy order
     elif user_input == SHOW:
         print("Actual shopping list: ")
-        for i in range(len(shopping_list)):
-            print("{} : {}".format(i+1,shopping_list[i]))
+        show_list(shopping_list)
+        
             
     #TODO User should be able to state DONE when it finish, so that he can print a list
     elif user_input == CONDITION:
         print("Your final shopping list: ")
-        for n in range(len(shopping_list)):
-            print("{} : {}".format(n+1,shopping_list[n]))
+        show_list(shopping_list)
         print("Thanks for using our app!")
     else:
         print("Przykro mi nie ma takiej komendy, proszę wybrać cos ponownie!")
